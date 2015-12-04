c17e80d95992d9cd8abc2a1f6ecbbf93199d9dc3
Brian Wynn <b_wynn@apple.com>
2015-12-04 15:58:56 -0800

    wrapped up chapter 19 and middleware examples covering user authentication.

---
+++ ch19/express_middleware.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+function queryRemover( req, res, next ) {
+  console.log("\nBefore URL: ");
+  console.log(req.url);
+  req.url = req.url.split('?')[0];
+  console.log("\nAfter URL: ");
+  console.log(req.url);
+  next();
+}
+
+app.use( queryRemover );
+app.get("/no/query", function() {
+  res.send("test");
+});
+
+app.listen(8080);
