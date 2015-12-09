104f6b78a37185801f65a21833c635beeede5c54
Brian Wynn <b_wynn@apple.com>
2015-12-08 16:48:22 -0800

    Added examples from the chapter to provide user authentication. Unfortunately, there are multiple generated from the code provided. Checking the authors github repo for the book shows different dependencies being utilized, code updated pretty drastically from the book itself. Makes troubleshooting this page a bit frustrating.

---
+++ user-authentication-ch26/routes.js
@@ -0,0 +1,48 @@
+var express = require('express');
+
+module.exports = function(app) {
+  app
+    .use('/static', express.static('./static'))
+
+    .get('/', function(req, res) {
+      // Session verify
+      if (req.session.userID) {
+        res.render('index', {msg: req.session.msg,
+                             username: req.session.msg });
+      } else {
+        req.session.msg = 'Access denied!';
+        res.redirect('/login');
+      }
+    })
+
+    .get('/user', function(req, res) {
+      if (req.session.userID) {
+        res.render('user', {msg: req.session.msg});
+      } else {
+        req.session.msg = 'Acces denied!';
+        res.redirect('/login');
+      }
+    })
+
+    .get('/signup', function(req, res) {
+      res.render('signup', {msg: req.session.msg});
+    })
+
+    .get('/login', function(req, res) {
+      res.render('login', {msg: req.session.msg});
+    })
+
+    .get('/logout', function(req, res) {
+      req.session.destroy(function() {
+        res.redirect('/login');
+      });
+    });
+
+    // Routes to interact with users controller and database
+    var users = require('./controllers/users_controller');
+    app.post('/signup', users.signup);
+    app.post('/user/update', users.updateUser);
+    app.post('/user/delete', users.deleteUser);
+    app.post('/login', users.login);
+    app.get('/user/profile', users.getUserProfile);
+};
